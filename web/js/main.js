be77acb12870deb5e6ace06c40a62c41ef4298a0
Corey Mason <corey.mason98@googlemail.com>
2016-10-27 16:20:59 -0700

    Modal for Device Selection

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,14 @@
 var particle = new Particle();
 var token;
 
-$( "#particle-form" ).submit(function() {
+$('.login-form a').click(function(){
+  //Toggle form
+  $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+$( "#particle-form" ).submit(function(event) {
+  //Prevent refresh
+  event.preventDefault();
   //process form and login with account
   var email = $("#email").val();
   var password = $("#password").val();
@@ -15,7 +22,7 @@ $( "#particle-form" ).submit(function() {
       //console.log('API call completed on promise resolve: ', data.body.access_token);
       $('.alert-particle-login').hide(50);
       token = data.body.access_token;
-      //need to select device (maybe a modal?)
+      selectDevice();
     },
     function(err) {
       //console.log('API call completed on promise fail: ', err);
@@ -28,13 +35,53 @@ $( "#particle-form" ).submit(function() {
   );
 });
 
-$( "#key-form" ).submit(function() {
+$( "#key-form" ).submit(function(event) {
+  //Prevent refresh
+  event.preventDefault();
   //process form and login with account
   var accessKey = $("#access-key").val();
   var remember = $("#particle-remember").val(); //TODO: Implement
-  //Verify accessKey then set token equal to it
+  //TODO: Verify accessKey
+  token = accessKey;
+  selectDevice();
 });
 
-$('.login-form a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
+function selectDevice() {
+  var deviceList = particle.listDevices({ auth: token });
+  deviceList.then(
+    function(devices){
+      //console.log('Devices: ', devices);
+      //Delete any html lefttover
+      $('#devices').empty();
+
+      //Add html for devices in modal
+      var i = 0;
+      devices.body.forEach(function (device) {
+        //console.log('Device: ', device);
+        var content = "";
+        var connected;
+
+        if(devices["connected"]) {
+          connected = 'Online';
+        } else {
+          connected = 'Offline';
+        }
+        if(i != 0) {
+          content += '<hr>';
+        }
+
+        content += '<div class="row"><div class="col-md-8 text-truncate">' + device.name +
+            '</div><div class="col-md-4">' + connected + '</div></div><div class="row">' +
+            '<div class="col-md-12"><small class="text-muted">ID: ' + device.id + '</small></div></div>';
+        $("#devices").append(content);
+        i++;
+      });
+
+      //Show modal
+      $('#devices-modal').modal();
+    },
+    function(err) {
+      console.log('List devices call failed: ', err); //TODO: Handle this error
+    }
+  );
+}
